1096a16596c55f17801180bc03a934ac6c5dc4a4
abi765 <56687824+abi765@users.noreply.github.com>
2024-10-17 11:10:31 +0000

    secrets.toml file modifed with some new parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,25 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+# Attempt to connect to Snowflake
+try:    # Remove this line
+    cnx = st.connection("snowflake")
+    session = cnx.session()
+    st.success("Connected to snowfalke successfully!") # Remove this line
+except Exception as e: # Remove this line
+    st.error(f"Error connecting to Snowflake: {str(e)}") # Remove this line
+    st.stop() # Remove this line
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 # import streamlit as st
 
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+
 # session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
